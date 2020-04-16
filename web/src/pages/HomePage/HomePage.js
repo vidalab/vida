0c5cbe1a2d790e53a4a5f569849c7bbfb89deb6b
Phuoc Do <dnprock@gmail.com>
2020-04-16 14:42:01 -0700

    Add Homepage

---
+++ web/src/pages/HomePage/HomePage.js
@@ -0,0 +1,21 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const HomePage = () => {
+  return (
+    <>
+      <header>
+        <h1>Redwood Viz</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>Home</main>
+    </>
+  )
+}
+
+export default HomePage
