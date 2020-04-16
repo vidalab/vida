f6ca01911e7971cd8b92310a34c35706673ac0e7
Phuoc Do <dnprock@gmail.com>
2020-04-16 14:44:18 -0700

    Incorporate tailwindcss

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div className="flex mt-6 justify-start md:justify-center xl:justify-start">
           <ul>
             <li>
-              <Link to={routes.about()}
+              <Link to={"/"}
                 className="rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-teal-500 hover:bg-teal-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md"
                 >
                 About</Link>
