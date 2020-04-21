b4609afbe910290b47f6624e560599991be023c0
Phuoc Do <dnprock@gmail.com>
2020-04-20 22:04:56 -0700

    Remove env variable logging

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -1,7 +1,6 @@
 import vizJson from '../viz/viz.json'
 
 export const getViz = ({ name }) =>  {
-  console.log(process.env.VIZ_JSON)
   if (process.env.VIZ_JSON) {
     const vJson = require('../viz/' + process.env.VIZ_JSON)
     return {
