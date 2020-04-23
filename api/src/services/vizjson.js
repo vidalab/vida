fce2ed712bd1e639673e52fe29ebf6c43676ea5d
Phuoc Do <dnprock@gmail.com>
2020-04-22 18:40:31 -0700

    Remove log

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -4,7 +4,6 @@ import fetch from "node-fetch"
 export const getViz = async ({ name }) =>  {
   if (process.env.VIZ_JSON) {
     const vJson = require("../viz/" + process.env.VIZ_JSON)
-    console.log(vJson["data"][0]["url"])
     vJson["data"].forEach(async (d) => {
       if (d["url"]) {
         // retrieve data from url to send to client
