3e778f3c1f8664f731277b92b0a115c07f407c50
Phuoc Do <dnprock@gmail.com>
2020-04-28 15:12:19 -0700

    Use for loop instead of forEach to avoid async wait issue with requesting url data

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -4,14 +4,14 @@ import fetch from "node-fetch"
 export const getViz = async ({ name }) =>  {
   if (process.env.VIZ_JSON) {
     const vJson = require("../viz/" + process.env.VIZ_JSON)
-    vJson["data"].forEach(async (d) => {
+    for (const d of vJson["data"]) {
       if (d["url"]) {
         // retrieve data from url to send to client
         const response = await fetch(d["url"])
         const json = await response.json()
         d["values"] = json
       }
-    })
+    }
     return {
       name: name,
       data: JSON.stringify(vJson)
