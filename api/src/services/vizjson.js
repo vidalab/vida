ae2b773cf1a93730db7637dfe8c9adea0b4805ed
Phuoc Do <dnprock@gmail.com>
2020-04-28 17:10:53 -0700

    Revert "Move viz dir inside services for deployment purpose"
    This reverts commit 9c6dc4b546ac3182a4380ed56c908774bd6b6a56.

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -1,9 +1,9 @@
-import vizJson from "./viz/viz.json"
+import vizJson from "../viz/viz.json"
 import fetch from "node-fetch"
 
 export const getViz = async ({ name }) =>  {
   if (process.env.VIZ_JSON) {
-    const vJson = require("./viz/" + process.env.VIZ_JSON)
+    const vJson = require("../viz/" + process.env.VIZ_JSON)
     for (const d of vJson["data"]) {
       if (d["url"]) {
         // retrieve data from url to send to client
