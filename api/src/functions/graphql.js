7a5da04109caaa6f7aef7a7f787eb16eb99f0803
Phuoc Do <dnprock@gmail.com>
2020-04-29 13:38:42 -0700

    Remove db references

--- api/src/functions/graphql.js
+++ api/src/functions/graphql.js
@@ -5,8 +5,6 @@ import {
 } from '@redwoodjs/api'
 import importAll from '@redwoodjs/api/importAll.macro'
 
-import { db } from 'src/lib/db'
-
 const schemas = importAll('api', 'graphql')
 const services = importAll('api', 'services')
 
@@ -14,6 +12,5 @@ export const handler = createGraphQLHandler({
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
-  }),
-  db,
+  })
 })
