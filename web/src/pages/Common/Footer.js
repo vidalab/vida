151d6bafbe766a36ea4673cb1f969aa51fd38d35
Phuoc Do <dnprock@gmail.com>
2020-04-29 16:15:59 -0700

    Add link to vida project

--- web/src/pages/Common/Footer.js
+++ web/src/pages/Common/Footer.js
@@ -8,7 +8,7 @@ const Footer = (props) => {
           </span>
 
           <span className="text-sm text-gray-700 float-right px-2 m-2">
-            Powered by <a href="https://redwoodjs.com/" target="_blank">Redwoodjs</a>
+            Powered by <a className="text-blue-500" href="https://github.com/vidalab/vida">vida</a> & <a className="text-blue-500" href="https://redwoodjs.com/" target="_blank">redwoodjs</a>
           </span>
         </div>
       </footer>
