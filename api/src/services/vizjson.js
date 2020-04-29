2b06c557ad4d230cfc3c4773efc613e1ccc2b1bb
Phuoc Do <dnprock@gmail.com>
2020-04-28 17:28:44 -0700

    Revert "Try move viz inside services again"
    This reverts commit 49bec8c50d2984b26dd1620507a44d2d5a8a37a7.

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -1,9 +1,9 @@
 import fetch from "node-fetch"
 
 export const getViz = async ({ name }) =>  {
-  let vJson = require("./viz/viz.json")
+  let vJson = require("../viz/viz.json")
   if (process.env.VIZ_JSON) {
-    vJson = require("./viz/" + process.env.VIZ_JSON)
+    vJson = require("../viz/" + process.env.VIZ_JSON)
     for (const d of vJson["data"]) {
       if (d["url"]) {
         // retrieve data from url to send to client
