698df980c7654cd91847ae6f06fe2ee5ed2f445a
Phuoc Do <dnprock@gmail.com>
2020-04-30 17:10:08 -0700

    Render homepage if VIZ_JSON environment variable is not set

--- api/src/services/vizjson.js
+++ api/src/services/vizjson.js
@@ -1,5 +1,5 @@
 export const getViz = ({ name }) =>  {
-  let vizName = process.env.VIZ_JSON ? process.env.VIZ_JSON : "viz.json"
+  let vizName = process.env.VIZ_JSON ? process.env.VIZ_JSON : ""
   return {
     name: name,
     vizName: vizName
--- web/src/components/VizCell.js
+++ web/src/components/VizCell.js
@@ -1,4 +1,6 @@
-import DataLoader from "./DataLoader";
+import DataLoader from "./DataLoader"
+import Header from '../pages/Common/Header'
+import Footer from '../pages/Common/Footer'
 
 export const QUERY = gql`
   query($name: String!) {
@@ -18,7 +20,21 @@ export const Empty = () => <div className={containerClassName + " text-center"}>
 export const Failure = ({ error }) => <div className={containerClassName + " text-center"}>Error: {error.message}</div>
 
 export const Success = ({ result }) => {
-  return (
-    <DataLoader vizName={result.vizName}/>
-  )
+  let cssStyle = {
+    height: "calc(100% - 100px)"
+  }
+  if (result.vizName != "") {
+    return (
+      <DataLoader vizName={result.vizName}/>
+    )
+  } else {
+    return (
+      <div style={{height: "100%"}}>
+        <Header name="Vida"/>
+        <div className={containerClassName} style={cssStyle}>
+        </div>
+        <Footer description="Flexible Data Visualization"/>
+      </div>
+    )
+  }
 }
