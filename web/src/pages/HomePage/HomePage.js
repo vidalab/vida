c960411f249f69d6df941aa76fc06e81b83f8122
Phuoc Do <dnprock@gmail.com>
2020-05-15 17:29:41 -0700

    Update marketing content for homepage

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,16 +5,36 @@ import DashboardsCell from '../../components/DashboardsCell/DashboardsCell'
 
 const HomePage = () => {
   let cssStyle = {
-    height: "calc(100% - 80px)"
   }
   return (
-    <div style={{height: "100%"}}>
+    <>
       <Header name="Vida"/>
+      <div className={containerClassName + " flex px-6 pt-10 pb-10"}>
+        <div className="w-1/2">
+          <h1 className="text-3xl tracking-tight sm:text-4xl md:text-5xl xl:text-4xl font-medium leading-tight">
+            Create compelling dashboards quickly.
+            <span className="sm:block text-teal-500 font-medium">No coding required.</span>
+          </h1>
+          <p className="mt-6 leading-relaxed sm:text-lg md:text-xl xl:text-lg text-gray-600">
+            Vida is a software tool that reads JSON files and turns them
+            into compelling dashboards. The JSON files allow you to customize data source, layout,
+            and charts.
+          </p>
+
+          <div className="flex mt-6 justify-start md:justify-center xl:justify-start">
+            <a href="#" className="rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-teal-500 hover:bg-teal-600 md:text-lg xl:text-base text-white font-semibold leading-tight shadow-md">Create Dashboard</a>
+            <a href="#" className="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 bg-white hover:bg-gray-200 md:text-lg xl:text-base text-gray-800 font-semibold leading-tight shadow-md">Publish Dashboard</a>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <img src="/images/HomePageLogo.png" />
+        </div>
+      </div>
       <div className={containerClassName} style={cssStyle}>
         <DashboardsCell />
       </div>
       <Footer description="Flexible Data Visualization"/>
-    </div>
+    </>
   )
 }
 
