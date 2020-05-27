137d289625332b83c351fa5737425f0b1c84159b
Phuoc Do <dnprock@gmail.com>
2020-05-27 11:04:14 -0700

    Add npm package build template

---
+++ package/src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class VidaComponent extends Component {
+    render () {
+        return (
+            <div>I am a Vida react npm module 2</div>
+        )
+    }
+}
