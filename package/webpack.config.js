fa2888aebb3d74634652d2bf4523846322f86050
Phuoc Do <dnprock@gmail.com>
2020-06-01 14:32:20 -0700

    Try different defnition of NODE_ENV

--- package/webpack.config.js
+++ package/webpack.config.js
@@ -26,7 +26,9 @@ module.exports = {
   mode: 'production',
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': 'production'
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
     })
   ]
 };
