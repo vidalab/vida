caffd5fab7a085740af62ccd2c9505ea5e549814
Phuoc Do <dnprock@gmail.com>
2021-08-26 15:16:57 -0700

    Type formatting

--- web/src/components/Charts/GrammarParser.tsx
+++ web/src/components/Charts/GrammarParser.tsx
@@ -1,11 +1,17 @@
-import React from "react"
-import NivoLineChart from "./NivoLineChart"
-import NivoBarChart from "./NivoBarChart"
-import NivoScatterChart from "./NivoScatterChart"
-import NivoPieChart from "./NivoPieChart"
-import NivoBubbleChart from "./NivoBubbleChart"
-import MarkdownText from "./MarkdownText"
-import { JSONVizData, XYAxes, JSONChartDataColumn, IHash, UrlData } from "./VizData"
+import React from 'react'
+import NivoLineChart from './NivoLineChart'
+import NivoBarChart from './NivoBarChart'
+import NivoScatterChart from './NivoScatterChart'
+import NivoPieChart from './NivoPieChart'
+import NivoBubbleChart from './NivoBubbleChart'
+import MarkdownText from './MarkdownText'
+import {
+  JSONVizData,
+  XYAxes,
+  JSONChartDataColumn,
+  IHash,
+  UrlData,
+} from './VizData'
 
 const GrammarParser = (jsonData: JSONVizData, urlData: UrlData) => {
   const getData = (name: string): object[] => {
@@ -29,72 +35,87 @@ const GrammarParser = (jsonData: JSONVizData, urlData: UrlData) => {
 
   const getNivoBubbleColors = (dataArray: any, axes: XYAxes): any => {
     // The first color is white for the outer bubble
-    let colors = ["#FFFFFF"]
-    let textColors = ["#FFFFFF"]
+    const colors = ['#FFFFFF']
+    const textColors = ['#FFFFFF']
     dataArray.forEach((d: any, index: number) => {
       colors.push(d[axes.x.color])
       textColors.push(d[axes.x.textColor])
     })
-    return {colors: colors, textColors: textColors}
+    return { colors: colors, textColors: textColors }
   }
 
-  const getNivoPieData = (data: any, group: any, value: any, colors: any): any => {
-    let dataPoints: any = []
+  const getNivoPieData = (
+    data: any,
+    group: any,
+    value: any,
+    colors: any
+  ): any => {
+    const dataPoints: any = []
     data.forEach((d: any, index: any) => {
-      dataPoints.push({"id": d[group], "label": d[group], "value": d[value], "color": colors[index % colors]})
+      dataPoints.push({
+        id: d[group],
+        label: d[group],
+        value: d[value],
+        color: colors[index % colors],
+      })
     })
     return dataPoints
   }
 
   const getNivoLineData = (dataArray: any, axes: XYAxes): any => {
-    let lineData: any = []
-    let colors: any = []
+    const lineData: any = []
+    const colors: any = []
     axes.y.dataColumns.forEach((yAxis: JSONChartDataColumn) => {
       colors.push(yAxis.color)
-      let data: any = []
-      let lData = {id: yAxis.name, color: yAxis.color, data: data}
+      const data: any = []
+      const lData = { id: yAxis.name, color: yAxis.color, data: data }
       dataArray.forEach((d: IHash) => {
         if (!isNaN(d[yAxis.name])) {
-          lData.data.push({x: d[axes.x.dataColumn], y: d[yAxis.name]})
+          lData.data.push({ x: d[axes.x.dataColumn], y: d[yAxis.name] })
         }
       })
       lineData.push(lData)
     })
-    return {data: lineData, colors: colors}
+    return { data: lineData, colors: colors }
   }
 
   const getNivoBarData = (dataArray: object[], axes: XYAxes): any => {
-    let barData: any = []
+    const barData: any = []
     dataArray.forEach((d: IHash) => {
-      let dp: IHash = {}
+      const dp: IHash = {}
       axes.y.dataColumns.forEach((yAxis) => {
         dp[axes.x.dataColumn] = d[axes.x.dataColumn]
         dp[yAxis.name] = d[yAxis.name]
-        dp[yAxis.name + "Color"] = yAxis.color
+        dp[yAxis.name + 'Color'] = yAxis.color
       })
       barData.push(dp)
     })
-    let keys: string[] = [], colors: string[] = []
+    const keys: string[] = [],
+      colors: string[] = []
     axes.y.dataColumns.forEach((yAxis: JSONChartDataColumn) => {
       keys.push(yAxis.name)
       colors.push(yAxis.color)
     })
-    return {data: barData, keys: keys, colors: colors}
+    return { data: barData, keys: keys, colors: colors }
   }
 
-  const getNivoScatterData = (dataArray: any[], axes: XYAxes, ids: string[]): any => {
-    let scatterData: any[] = []
+  const getNivoScatterData = (
+    dataArray: any[],
+    axes: XYAxes,
+    ids: string[]
+  ): any => {
+    const scatterData: any[] = []
     dataArray.forEach((dArray: any[], i: number) => {
-      let sData: any[] = []
+      const sData: any[] = []
       dArray.forEach((d: any, j: number) => {
         sData.push({
           x: d[axes.x.dataColumn],
-          y: d[axes.y.dataColumn]
+          y: d[axes.y.dataColumn],
         })
       })
       scatterData.push({
         id: ids[i],
-        data: sData
+        data: sData,
       })
     })
 
@@ -103,15 +124,17 @@ const GrammarParser = (jsonData: JSONVizData, urlData: UrlData) => {
 
   const getVizInfo = (): any => {
     return {
-      "name": jsonData["name"], "description": jsonData["description"],
-      "scripts": jsonData["scripts"],
-      "header": jsonData["header"]}
+      name: jsonData['name'],
+      description: jsonData['description'],
+      scripts: jsonData['scripts'],
+      header: jsonData['header'],
+    }
   }
 
-  let els: JSX.Element[] = []
+  const els: JSX.Element[] = []
   const containerCssStyle = {
-    width: "100%",
-    height: "100%"
+    width: '100%',
+    height: '100%',
   }
   jsonData.charts.forEach((chart, index) => {
     const chartType = chart.type
@@ -119,119 +142,175 @@ const GrammarParser = (jsonData: JSONVizData, urlData: UrlData) => {
     let data
     if (Array.isArray(dataName)) {
       data = []
-      for (let d of dataName) {
+      for (const d of dataName) {
         data.push(getData(d))
       }
     } else {
       data = getData(dataName)
     }
     const colX = chart.position.x + 1,
-          colSpan = chart.position.columns,
-          rowSpan = chart.position.rows,
-          colXClass = "col-start-" + colX,
-          colSpanClass = "col-span-" + colSpan,
-          rowSpanClass = "row-span-" + rowSpan
+      colSpan = chart.position.columns,
+      rowSpan = chart.position.rows,
+      colXClass = 'col-start-' + colX,
+      colSpanClass = 'col-span-' + colSpan,
+      rowSpanClass = 'row-span-' + rowSpan
     let el: JSX.Element = null
-    if (chartType == "line") {
+    if (chartType == 'line') {
       const lineData = getNivoLineData(data, chart.axes)
-      el = <div key={"line-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoLineChart
-                key={"line-chart-" + index}
-                chartTitle={chart.title}
-                axes={chart.axes}
-                legend={chart.legend}
-                colors={lineData.colors}
-                data={lineData.data} />
-            </div>
-    } else if (chartType == "bar") {
+      el = (
+        <div
+          key={'line-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoLineChart
+            key={'line-chart-' + index}
+            chartTitle={chart.title}
+            axes={chart.axes}
+            legend={chart.legend}
+            colors={lineData.colors}
+            data={lineData.data}
+          />
+        </div>
+      )
+    } else if (chartType == 'bar') {
       const barData = getNivoBarData(data, chart.axes)
-      el = <div key={"bar-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoBarChart
-                key={"bar-chart-" + index}
-                chartTitle={chart.title}
-                axes={chart.axes}
-                keys={barData.keys}
-                legend={chart.legend}
-                colors={barData.colors}
-                data={barData.data} />
-            </div>
-    } else if (chartType == "horizontal-bar") {
+      el = (
+        <div
+          key={'bar-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoBarChart
+            key={'bar-chart-' + index}
+            chartTitle={chart.title}
+            axes={chart.axes}
+            keys={barData.keys}
+            legend={chart.legend}
+            colors={barData.colors}
+            data={barData.data}
+          />
+        </div>
+      )
+    } else if (chartType == 'horizontal-bar') {
       const barData = getNivoBarData(data, chart.axes)
-      el = <div key={"horizontal-bar-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoBarChart
-                key={"horizontal-bar-chart-" + index}
-                chartTitle={chart.title}
-                horizontal={true}
-                axes={chart.axes}
-                keys={barData.keys}
-                legend={chart.legend}
-                colors={barData.colors}
-                data={barData.data} />
-            </div>
-    } else if (chartType == "scatter") {
-      const scatterData = getNivoScatterData(data, chart.axes, dataName as string[])
-      el = <div key={"scatter-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoScatterChart
-                key={"scatter-chart-" + index}
-                axes={chart.axes}
-                colors={chart.colors}
-                data={scatterData} />
-            </div>
-    } else if (chartType == "area") {
+      el = (
+        <div
+          key={'horizontal-bar-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoBarChart
+            key={'horizontal-bar-chart-' + index}
+            chartTitle={chart.title}
+            horizontal={true}
+            axes={chart.axes}
+            keys={barData.keys}
+            legend={chart.legend}
+            colors={barData.colors}
+            data={barData.data}
+          />
+        </div>
+      )
+    } else if (chartType == 'scatter') {
+      const scatterData = getNivoScatterData(
+        data,
+        chart.axes,
+        dataName as string[]
+      )
+      el = (
+        <div
+          key={'scatter-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoScatterChart
+            key={'scatter-chart-' + index}
+            axes={chart.axes}
+            colors={chart.colors}
+            data={scatterData}
+          />
+        </div>
+      )
+    } else if (chartType == 'area') {
       const lineData = getNivoLineData(data, chart.axes)
-      el = <div key={"line-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoLineChart
-                key={"line-chart-" + index}
-                chartTitle={chart.title}
-                enableArea={true}
-                axes={chart.axes}
-                colors={lineData.colors}
-                data={lineData.data} />
-            </div>
-    } else if (chartType == "pie") {
-      const pieData = getNivoPieData(data, chart.group, chart.value, chart.colors)
-      el = <div key={"pie-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoPieChart
-                key={"pie-chart-" + index}
-                data={pieData} />
-          </div>
-    } else if (chartType == "bubble") {
+      el = (
+        <div
+          key={'line-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoLineChart
+            key={'line-chart-' + index}
+            chartTitle={chart.title}
+            enableArea={true}
+            axes={chart.axes}
+            colors={lineData.colors}
+            data={lineData.data}
+          />
+        </div>
+      )
+    } else if (chartType == 'pie') {
+      const pieData = getNivoPieData(
+        data,
+        chart.group,
+        chart.value,
+        chart.colors
+      )
+      el = (
+        <div
+          key={'pie-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoPieChart key={'pie-chart-' + index} data={pieData} />
+        </div>
+      )
+    } else if (chartType == 'bubble') {
       const bubbleColorData = getNivoBubbleColors(data, chart.axes)
-      el = <div key={"bubble-chart-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <NivoBubbleChart
-                key={"bubble-chart-" + index}
-                colors={bubbleColorData.colors}
-                textColors={bubbleColorData.textColors}
-                axes={chart.axes}
-                data={data} />
-            </div>
-    } else if (chartType == "markdown") {
-      el = <div key={"markdown-container-" + index} style={containerCssStyle}
-              className={colXClass + " " + colSpanClass + " " + rowSpanClass}
-            >
-              <MarkdownText text={chart.text} />
-            </div>
+      el = (
+        <div
+          key={'bubble-chart-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <NivoBubbleChart
+            key={'bubble-chart-' + index}
+            colors={bubbleColorData.colors}
+            textColors={bubbleColorData.textColors}
+            axes={chart.axes}
+            data={data}
+          />
+        </div>
+      )
+    } else if (chartType == 'markdown') {
+      el = (
+        <div
+          key={'markdown-container-' + index}
+          style={containerCssStyle}
+          className={colXClass + ' ' + colSpanClass + ' ' + rowSpanClass}
+        >
+          <MarkdownText text={chart.text} />
+        </div>
+      )
     }
     els.push(el)
   })
-  let gridEl = <div
-    className={"grid grid-cols-" + jsonData.columns + " grid-rows-" + jsonData.rows + " gap-4"}
-    style={containerCssStyle}>{els}</div>
-  return {charts: gridEl, vizInfo: getVizInfo()}
+  const gridEl = (
+    <div
+      className={
+        'grid grid-cols-' +
+        jsonData.columns +
+        ' grid-rows-' +
+        jsonData.rows +
+        ' gap-4'
+      }
+      style={containerCssStyle}
+    >
+      {els}
+    </div>
+  )
+  return { charts: gridEl, vizInfo: getVizInfo() }
 }
 
 export default GrammarParser
